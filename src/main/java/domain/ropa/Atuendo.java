e868b2915d5119c0dabe9a93651fa387fc82a074
sbenedetti21 <sbenedetti@est.frba.utn.edu.ar>
2021-04-19 12:46:19 -0300

    agrego constructor en atuendo

--- src/main/java/domain/ropa/Atuendo.java
+++ src/main/java/domain/ropa/Atuendo.java
@@ -1,7 +1,13 @@
 package domain.ropa;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Atuendo {
-  private List<Prenda> prendas;
+  private List<Prenda> prendas = new ArrayList<>();
+
+  public Atuendo(Prenda ... prendas) {
+    Collections.addAll(this.prendas, prendas);
+  }
 }
