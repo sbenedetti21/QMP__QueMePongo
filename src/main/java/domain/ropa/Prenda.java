c177c7a7f8a76d7a0392f67fce7c92b93a78b9ee
sbenedetti21 <sbenedetti@est.frba.utn.edu.ar>
2021-04-21 14:01:33 -0300

    final

--- src/main/java/domain/ropa/Prenda.java
+++ src/main/java/domain/ropa/Prenda.java
@@ -1,6 +1,7 @@
 package domain.ropa;
 
 public class Prenda {
+
   private String tipoDePrenda;
   private Categoria categoria;
   private String material;
@@ -13,4 +14,12 @@ public class Prenda {
     this.material = material;
     this.colorPrincipal = colorPrincipal;
   }
+
+
+  // Esta es la manera que se me ocurrio de añadir un color secundario, aunque podría traer problemas ya que
+  // si no se utilizase, colorSecundario quedaría en null
+  public void setColorSecundario(String colorSecundario) {
+    this.colorSecundario = colorSecundario;
+  }
+
 }
